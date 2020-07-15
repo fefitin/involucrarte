c1c1d4fd7bf52e10c5c343189e748c9fbed5a18a
Federico García <fede@divinodi.com>
2020-07-15 17:49:20 -0300

    Ajustes home

--- components/FundacionSi.js
+++ components/FundacionSi.js
@@ -2,7 +2,9 @@ export default function FundacionSi() {
   return (
     <div className="container fundacion">
       <p>El 100% del dinero obtenido de la venta se destina a:</p>
-      <img src="/images/fundacionsi.png" alt="FundacionSí" />
+      <a href="https://fundacionsi.org.ar/" target="_blank">
+        <img src="/images/fundacionsi.png" alt="FundacionSí" />
+      </a>
     </div>
   );
 }
