a0dce5f746581d3cc875994f20ec38bb7c177542
Federico García <fede@divinodi.com>
2020-08-13 13:07:32 -0300

    Breadcrumbs con links.

--- components/Breadcrumb.js
+++ components/Breadcrumb.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import API from './../services/API';
+import Link from 'next/link';
 
 const Breadcrumb = () => {
   const router = useRouter();
@@ -10,7 +11,9 @@ const Breadcrumb = () => {
     updateBreadcrumb(router.pathname, router.query, setPages);
   }, [router.pathname]);
 
-  return <p className="breadcrumb">. {pages.join(' › ')}</p>;
+  return pages.length ? (
+    <p className="breadcrumb">.{pages.reduce((prev, curr) => [prev, ' › ', curr])}</p>
+  ) : null;
 };
 
 const updateBreadcrumb = async (path, query, setPages) => {
@@ -33,8 +36,10 @@ const updateBreadcrumb = async (path, query, setPages) => {
   }
 
   //Replace :obra with artwork title
+  let obra = null;
+
   if (query && query.id) {
-    const obra = await API.get(`/obras/${query.id}`);
+    obra = await API.get(`/obras/${query.id}`);
     breadcrumb = breadcrumb.map(page => {
       if (page === ':obra') {
         page = obra.obra.titulo;
@@ -43,6 +48,26 @@ const updateBreadcrumb = async (path, query, setPages) => {
       return page;
     });
   }
+
+  //Create links
+  path = path.split('/').slice(1);
+  breadcrumb = breadcrumb.map((page, i) => {
+    const thisPath = '/' + path.slice(0, i + 1).join('/');
+    let thisPathAs = thisPath;
+    if (obra) {
+      thisPathAs = thisPathAs.replace('[id]', obra.obra.slug);
+    }
+
+    if (i === breadcrumb.length - 1) {
+      return page;
+    } else {
+      return (
+        <Link key={i} href={thisPath} as={thisPathAs}>
+          <a>{page}</a>
+        </Link>
+      );
+    }
+  });
 
   setPages(breadcrumb);
 };
